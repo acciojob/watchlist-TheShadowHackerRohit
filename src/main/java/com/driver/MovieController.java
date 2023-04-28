0bbdef62128e45e9e83aa07d9cd6a2239c1919ff
rohit <rohitme0091@gmail.com>
2023-04-28 12:20:15 +0530

    Copied and changes

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -54,7 +54,7 @@ public class MovieController {
     }
 
     @GetMapping("/get-all-movies")
-    public ResponseEntity getAllMovies() {
+    public ResponseEntity findAllMovies() {
         List<Movie> movies = movieService.getAllMovies();
         return new ResponseEntity(movies, HttpStatus.OK);
     }
